ecd8c6f2c8e86902ce11312d4de49d19375ea4d7
Roman Tolstopyatov <r.tolstopyatov@gmail.com>
2020-10-27 21:58:39 +0400

    lesson 2 - login and sample

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const http = require('http');
 const PORT = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('X-Author', 'Roman83');
-  res.end('Roman83');
+  if (req.url === '/login/') {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('X-Author', 'Roman83');
+    res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
+    res.end('Roman83');
+  } else if (req.url === '/sample/') {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('X-Author', 'Roman83');
+    res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
+    res.end('function task(x) { return x * (this**2); }');
+  }
 });
 
 server.listen(PORT, () => {
