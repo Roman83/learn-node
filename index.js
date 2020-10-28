07852b1ac015517b6aebea63efadb4a8f47b8c9b
Roman Tolstopyatov <r.tolstopyatov@gmail.com>
2020-10-28 14:40:20 +0400

    task 3, отправка html, рефакторинг

--- index.js
+++ index.js
@@ -1,21 +1,64 @@
 const http = require('http');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 8080;
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/login/') {
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('X-Author', 'Roman83');
-    res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
-    res.end('Roman83');
-  } else if (req.url === '/sample/') {
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('X-Author', 'Roman83');
-    res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
-    res.end('function task(x) { return x * (this**2); }');
+function setHeaders(res, params = {}) {
+  const headers = Object.entries({
+    'Access-Control-Allow-Origin': '*',
+    'X-Author': 'Roman83',
+    'Content-Type': 'text/plain; charset=UTF-8',
+    ...params,
+  });
+  for (const [key, value] of headers) {
+    res.setHeader(key, value);
   }
+}
+
+const server = http.createServer((req, res) => {
+  switch (req.url) {
+  case '/login/': 
+    res.statusCode = 200;
+    setHeaders(res);
+    res.end('Roman83');
+    break;
+  case '/sample/':
+    res.statusCode = 200;
+    setHeaders(res);
+    res.end('function task(x) { return x * (this**2); }');
+    break
+  case '/promise/':
+    res.statusCode = 200;
+    setHeaders(res);
+    res.end('function task(x) { return new Promise((res, rej) => { if (x < 18) { res("yes"); } else { rej("no"); }}); }');
+    break;
+  case '/fetch/':
+    fs.readFile('fetch.html', (err, result) => {
+      if (err) {
+        res.statusCode = 500;
+        setHeaders(res);
+        res.end();
+      } else {
+        res.statusCode = 200;
+        setHeaders(res, { 'Content-Type': 'text/html; charset=UTF-8' });
+        res.end(result);
+      }
+    });
+    break;
+  default:
+    fs.readFile('index.html', (err, result) => {
+      if (err) {
+        res.statusCode = 500;
+        setHeaders(res);
+        res.end();
+      } else {
+        res.statusCode = 200;
+        setHeaders(res, { 'Content-Type': 'text/html; charset=UTF-8' });
+        res.end(result);
+      }
+    });
+  }
+  
 });
 
 server.listen(PORT, () => {
